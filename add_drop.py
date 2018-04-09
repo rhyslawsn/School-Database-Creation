420bedba65e96671ef20e8d35fe8554c0a86f1ab
Rhys Lawson <rhysl@dblinux1.csc.uvic.ca>
2018-04-08 23:12:41 -0700

    Bunch of changes to the python files

--- add_drop.py
+++ add_drop.py
@@ -13,6 +13,15 @@ if len(sys.argv) < 2:
 input_filename = sys.argv[1]
 
 # Open your DB connection here
+psql_user = 'rhysl' #Change this to your username
+psql_db = 'rhysl' #Change this to your personal DB name
+psql_password = 'V00818835' #Put your password (as a string) here
+psql_server = 'studdb1.csc.uvic.ca'
+psql_port = 5432
+
+conn = psycopg2.connect(dbname=psql_db,user=psql_user,password=psql_password,host=psql_server,port=psql_port)
+cursor = conn.cursor()
+		
 
 with open(input_filename) as f:
 	for row in csv.reader(f):
@@ -25,34 +34,26 @@ with open(input_filename) as f:
 		add_or_drop,student_id,student_name,course_code,term = row
 		
 		#Do something with the data here
-		#Make sure to catch any exceptions that occur and roll back the transaction if a database error occurs.
-		
-		if add_or_drop == 'ADD' or add_or_drop == 'DROP':
-			pass # Nothing to see here folks ...
-		else:
-			print("Error: Invalid input for add_or_drop")
-			print(row)
-			#Maybe abort the active transaction and roll back at this point?
-			break
-		if len(student_id) != 9:
-			print("Error: Invalid input for add_or_drop")
-			print(row)
-			#Maybe abort the active transaction and roll back at this point?
-			break
-		if len(student_name) > 255:
-			print("Error: Invalid input for add_or_drop")
-			print(row)
-			#Maybe abort the active transaction and roll back at this point?
-			break
-		if len(course_code) > 10:
-			print("Error: Invalid input for add_or_drop")
-			print(row)
-			#Maybe abort the active transaction and roll back at this point?
-			break
-		if term[-2:] == '01' or term[-2:] == '05' or term[-2:] == '09':
-			pass # Nothing to see here folks ...
-		else:
-			print("Error: Invalid term number")
-			print(row)
-			#Maybe abort the active transaction and roll back at this point?
-			break
+		try:
+			cursor.execute("insert into students values(%s,%s)",(student_id,student_name))
+			if add_or_drop == 'ADD':
+				cursor.execute("insert into enrollments values(%s,%s,%s,%s)",(student_id,course_code,term,None))
+			elif add_or_drop == 'DROP':
+				cursor.execute("delete from enrollments where student_id = %s and course_code = %s and term_code = %s",(student_id,course_code,term))
+		except psycopg2.ProgrammingError as err:
+                        #ProgrammingError is thrown when the database error is related to the format of the query (e.g. syntax error)
+                        print("Caught a ProgrammingError:",file=sys.stderr)
+                        print(err,file=sys.stderr)
+                        conn.rollback()
+		except psycopg2.IntegrityError as err:
+                        #IntegrityError occurs when a constraint (primary key, foreign key, check constraint or trigger constraint) is violated.
+                        print("Caught an IntegrityError:",file=sys.stderr)
+                        print(err,file=sys.stderr)
+                        conn.rollback()
+		except psycopg2.InternalError as err:
+                        #InternalError generally represents a legitimate connection error, but may occur in conjunction with user defined functions.
+                        #In particular, InternalError occurs if you attempt to continue using a cursor object after the transaction has been aborted.
+                        #(To reset the connection, run conn.rollback() and conn.reset(), then make a new cursor)
+                        print("Caught an IntegrityError:",file=sys.stderr)
+                        print(err,file=sys.stderr)
+                        conn.rollback()
--- create_courses.py
+++ create_courses.py
@@ -12,20 +12,23 @@ if len(sys.argv) < 2:
 	
 input_filename = sys.argv[1]
 
-# Open your DB connection here
-try:
-    os.system("ssh rhysl@studdb1.csc.uvic.ca -L -p 22")
-except:
-    print("Error: Unable to connect to the database")
+psql_user = 'rhysl' #Change this to your username
+psql_db = 'rhysl' #Change this to your personal DB name
+psql_password = 'V00818835' #Put your password (as a string) here
+psql_server = 'studdb1.csc.uvic.ca'
+psql_port = 5432
+
+conn = psycopg2.connect(dbname=psql_db,user=psql_user,password=psql_password,host=psql_server,port=psql_port)
+cursor = conn.cursor()
 
 with open(input_filename) as f:
-	print("Opened: " + input_filename)
 	for row in csv.reader(f):
 		if len(row) == 0:
 			continue #Ignore blank rows
 		if len(row) < 4:
 			print("Error: Invalid input line \"%s\""%(','.join(row)), file=sys.stderr)
 			#Maybe abort the active transaction and roll back at this point?
+			conn.rollback();
 			break
 		code, name, term, instructor, capacity = row[0:5]
 		prerequisites = row[5:] #List of zero or more items
@@ -33,36 +36,27 @@ with open(input_filename) as f:
 		#Do something with the data here
 		#Make sure to catch any exceptions that occur and roll back the transaction if a database error occurs.
 		
-		# Catch constraint violations here
-		if len(code) > 10:
-			print("Error: Invalid input for code")
-			print(row)
-			#Maybe abort the active transaction and roll back at this point?
-			break
-		if len(name) < 1 or len(name) > 128:
-			print("Error: Invalid input for name")
-			print(row)
-			#Maybe abort the active transaction and roll back at this point?
-			break
-		if len(instructor) < 1:
-			print("Error: Invalid input for instructor")
-			print(row)
-			#Maybe abort the active transaction and roll back at this point?
-			break
-		if len(instructor) < 1:
-			print("Error: Invalid input for instructor")
-			print(row)
-			#Maybe abort the active transaction and roll back at this point?
-			break
-		if int(capacity) < 1:
-			print("Error: Invalid input for capacity")
-			print(row)
-			#Maybe abort the active transaction and roll back at this point?
-			break
-		if term[-2:] == '01' or term[-2:] == '05' or term[-2:] == '09':
-			pass # Nothing to see here folks ...
-		else:
-			print("Error: Invalid term number")
-			print(row)
-			#Maybe abort the active transaction and roll back at this point?
-			break
+		try:
+			cursor.execute("insert into courses values(%s);",(code,))
+			cursor.execute("insert into course_offerings values(%s,%s,%s,%s,%s)",(code,name,term,capacity,instructor))
+			if prerequisites:
+                               for val in prerequisites:
+                                       cursor.execute("insert into prereqs values(%s,%s,%s)",(code,term,val))
+			conn.commit() #Only commit if no error occurs (commit will actually be prevented if an error occurs anyway)
+		except psycopg2.ProgrammingError as err: 
+			#ProgrammingError is thrown when the database error is related to the format of the query (e.g. syntax error)
+			print("Caught a ProgrammingError:",file=sys.stderr)
+			print(err,file=sys.stderr)
+			conn.rollback()
+		except psycopg2.IntegrityError as err: 
+			#IntegrityError occurs when a constraint (primary key, foreign key, check constraint or trigger constraint) is violated.
+			print("Caught an IntegrityError:",file=sys.stderr)
+			print(err,file=sys.stderr)
+			conn.rollback()
+		except psycopg2.InternalError as err:  
+			#InternalError generally represents a legitimate connection error, but may occur in conjunction with user defined functions.
+			#In particular, InternalError occurs if you attempt to continue using a cursor object after the transaction has been aborted.
+			#(To reset the connection, run conn.rollback() and conn.reset(), then make a new cursor)
+			print("Caught an IntegrityError:",file=sys.stderr)
+			print(err,file=sys.stderr)
+			conn.rollback()
